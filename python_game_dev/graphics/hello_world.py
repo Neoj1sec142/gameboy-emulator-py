f4df0ce5f1ad7a94a0759c1f95cd059333900ecb
Mark Harmon <neothej1sec@gmail.com>
2022-09-07 21:42:26 -0500

    working on the into to pygame with graphics

---
+++ python_game_dev/graphics/hello_world.py
@@ -0,0 +1,35 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+window = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello World!')
+
+# Set up the colors
+BLK = (0, 0, 0)
+WHT = (255, 255, 255)
+RED = (255, 0, 0)
+GRN = (0, 255, 0)
+BLU = (0, 0, 255)
+
+# Set up fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+# Set up the text
+text = basicFont.render('Hello World!', True, WHT, BLU)
+textRect = text.get_rect()
+textRect.centerx = window.get_rect().centerx
+textRect.centery = window.get_rect().centery
+
+# Draw the white background onto the surface
+window.fill(WHT)
+
+# DRaw a green polygon on to the surface
+pygame.draw.polygon(window, GRN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+
+# Draw a some bllue lines on the surface
+pygame.draw.line(window, BLU, (60, 60), (120, 60), 4)
+pygame.draw.line(window, BLU, (120, 60), (60, 120))
+pygame.draw.line(window, BLU, (60, 120), (120, 120), 4)
+
